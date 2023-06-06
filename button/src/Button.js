6a85f9b82abc77831e4074bba3bd4bb5b8cfd6ee
Abhirami2k41 <abhiramik@unifo.in>
2023-06-06 18:25:53 +0530

    buttons

---
+++ button/src/Button.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import './build/css/_variables.css';
+
+function Button() {
+  return (
+    <div className="App">
+      <button className="custombutton">Button-1</button>
+    </div>
+  );
+}
+
+export default Button;
---
+++ button/src/Buttonn.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import './build/css/_variables.css';
+
+function Buttonn() {
+    return (
+      <div className="App">
+        <button className="custombuttonn">Button-2</button>
+      </div>
+    );
+  }
+
+  export default Buttonn;
